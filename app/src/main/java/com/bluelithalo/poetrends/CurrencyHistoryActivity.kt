55037d4de9b075d77c939b8fd6335fb7d9185473
Ashwin Chakilum <azureemulation@gmail.com>
2019-08-19 01:58:07 -0500

    Beta build finalizing

--- app/src/main/java/com/bluelithalo/poetrends/CurrencyHistoryActivity.kt
+++ app/src/main/java/com/bluelithalo/poetrends/CurrencyHistoryActivity.kt
@@ -1,12 +1,14 @@
 package com.bluelithalo.poetrends
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -18,9 +20,21 @@ import com.bluelithalo.poetrends.poe_ninja.CurrencyHistoryViewModel
 import com.bluelithalo.poetrends.poe_ninja.CurrencyHistoryViewModelFactory
 import com.bluelithalo.poetrends.view.CurrencyViewHolder
 import com.bluelithalo.poetrends.view.FragmentViewHolder
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.google.gson.Gson
 
 import kotlinx.android.synthetic.main.activity_currency_history.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CurrencyHistoryActivity : AppCompatActivity()
 {
@@ -41,6 +55,12 @@ class CurrencyHistoryActivity : AppCompatActivity()
     var wikiButton: Button? = null
     var searchBuyButton: Button? = null
     var searchSellButton: Button? = null
+
+    var lineChart: LineChart? = null
+    var buyDateTextView: TextView? = null
+    var buyValueTextView: TextView? = null
+    var sellDateTextView: TextView? = null
+    var sellValueTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -66,9 +86,11 @@ class CurrencyHistoryActivity : AppCompatActivity()
             Overview.Type.FRAGMENT -> configureFragmentButtons(leagueId, currencyLine, poeTradeId)
         }
 
+        configureCurrencyDatumDisplay(currencyLine)
+
         currencyHistoryViewModel = ViewModelProviders.of(this, CurrencyHistoryViewModelFactory(leagueId, currencyType, currencyLine?.pay?.payCurrencyId ?: 0)).get(CurrencyHistoryViewModel::class.java)
         currencyHistoryViewModel.getCurrencyHistory().observe(this, Observer<CurrencyHistory> { currencyHistory ->
-            Log.i("CurrencyHistory", Gson().toJson(currencyHistory))
+            configureLineChart(currencyHistory)
         })
     }
 
@@ -114,7 +136,7 @@ class CurrencyHistoryActivity : AppCompatActivity()
         {
             override fun onClick(v: View?)
             {
-                val tradeCurrencyId = if (poeTradeId == 24 || poeTradeId == 520 || poeTradeId == 25) 6 else 4 // for expensive currency items: mirror, mirror shard, eternal orb
+                val tradeCurrencyId = if (poeTradeId == 24 || poeTradeId == 520 || poeTradeId == 25) 6 else 4 // for expensive currency currencys: mirror, mirror shard, eternal orb
                 val searchBuyUrl: String = "http://currency.poe.trade/search?league=${leagueId}&online=x&stock=&want=${poeTradeId}&have=${tradeCurrencyId}"
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(searchBuyUrl))
                 intent.resolveActivity(packageManager)?.let { startActivity(intent) }
@@ -126,7 +148,7 @@ class CurrencyHistoryActivity : AppCompatActivity()
         {
             override fun onClick(v: View?)
             {
-                val tradeCurrencyId = if (poeTradeId == 24 || poeTradeId == 520 || poeTradeId == 25) 6 else 4 // for expensive currency items: mirror, mirror shard, eternal orb
+                val tradeCurrencyId = if (poeTradeId == 24 || poeTradeId == 520 || poeTradeId == 25) 6 else 4 // for expensive currency currencys: mirror, mirror shard, eternal orb
                 val searchSellUrl: String = "http://currency.poe.trade/search?league=${leagueId}&online=x&stock=&want=${tradeCurrencyId}&have=${poeTradeId}"
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(searchSellUrl))
                 intent.resolveActivity(packageManager)?.let { startActivity(intent) }
@@ -170,5 +192,171 @@ class CurrencyHistoryActivity : AppCompatActivity()
                 intent.resolveActivity(packageManager)?.let { startActivity(intent) }
             }
         })
+    }
+
+    private fun configureCurrencyDatumDisplay(currencyLine: Line)
+    {
+        buyDateTextView = findViewById<TextView>(R.id.currency_buy_date_text_view)
+        buyValueTextView = findViewById<TextView>(R.id.currency_buy_value_text_view)
+        sellDateTextView = findViewById<TextView>(R.id.currency_sell_date_text_view)
+        sellValueTextView = findViewById<TextView>(R.id.currency_sell_value_text_view)
+
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        val buyValue = (currencyLine.receive?.value ?: 0.0)
+        val sellValue = 1.0 / (currencyLine.pay?.value ?: 0.0)
+
+        val dateString = "${calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH)} ${calendar.get(
+            Calendar.DAY_OF_MONTH)}"
+        val buyValueString = "${String.format("%.1f", buyValue)} ×"
+        val sellValueString = "${String.format("%.1f", sellValue)} ×"
+
+        buyDateTextView?.text = dateString
+        buyValueTextView?.text = buyValueString
+        sellDateTextView?.text = dateString
+        sellValueTextView?.text = sellValueString
+    }
+
+    private fun configureLineChart(currencyHistory: CurrencyHistory)
+    {
+        val buyValues: ArrayList<Entry> = ArrayList()
+        for (i in 0 until (currencyHistory.receiveCurrencyGraphData?.size ?: 0))
+        {
+            currencyHistory.receiveCurrencyGraphData?.get(i)?.let {
+                val daysAgo: Float = -1 * (it.daysAgo ?: 0).toFloat()
+                val currencyValue: Float = (it.value ?: 0.0).toFloat()
+                buyValues.add(Entry(daysAgo, currencyValue))
+            }
+        }
+
+        val sellValues: ArrayList<Entry> = ArrayList()
+        for (i in 0 until (currencyHistory.payCurrencyGraphData?.size ?: 0))
+        {
+            currencyHistory.payCurrencyGraphData?.get(i)?.let {
+                val daysAgo: Float = -1 * (it.daysAgo ?: 0).toFloat()
+                val currencyValue: Float = 1.0f / (it.value ?: 0.0).toFloat()
+                sellValues.add(Entry(daysAgo, currencyValue))
+            }
+        }
+
+        lineChart = findViewById<LineChart>(R.id.currency_line_chart)
+        lineChart?.let {
+
+            it.setExtraOffsets(0.0f, 0.0f, 0.0f, 8.0f)
+            it.setBackgroundColor(Color.rgb(40, 40, 40))
+            it.description.isEnabled = false
+            it.setTouchEnabled(true)
+            it.setDragEnabled(true)
+            it.setScaleEnabled(true)
+            it.setPinchZoom(false)
+            it.setDrawGridBackground(false)
+            it.setMaxHighlightDistance(50.0f)
+
+            val x = it.xAxis
+            x.setLabelCount(6, false)
+            x.textColor = Color.WHITE
+            x.setPosition(XAxis.XAxisPosition.BOTTOM)
+            x.setDrawGridLines(true)
+            x.gridColor = Color.rgb(125, 125, 125)
+            x.axisLineColor = Color.WHITE
+            x.setValueFormatter(object : IndexAxisValueFormatter()
+            {
+                override fun getFormattedValue(value: Float): String
+                {
+                    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                    val daysAgo = value.toInt()
+                    calendar.add(Calendar.DATE, daysAgo)
+                    return "${calendar.get(Calendar.MONTH)+1}/${calendar.get(Calendar.DAY_OF_MONTH)}"
+                }
+            })
+
+            val y = it.axisRight
+            y.setLabelCount(6, false)
+            y.textColor = Color.WHITE
+            y.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+            y.setDrawGridLines(true)
+            y.gridColor = Color.rgb(125, 125, 125)
+            y.axisLineColor = Color.WHITE
+
+            it.axisLeft.isEnabled = false
+            it.legend.isEnabled = false
+
+            val buyDataset = LineDataSet(buyValues, "Currency Buy History Dataset")
+            buyDataset.cubicIntensity = 0.2f
+            buyDataset.setDrawFilled(true)
+            buyDataset.lineWidth = 2.0f
+            buyDataset.color = Color.rgb(250, 200, 0)
+            buyDataset.setCircleColor(Color.rgb(250, 200, 0))
+            buyDataset.circleHoleColor = Color.rgb(250, 200, 0)
+            buyDataset.highLightColor = Color.rgb(0, 255, 0)
+            buyDataset.fillColor = Color.rgb(75, 50, 0)
+
+            val sellDataset = LineDataSet(sellValues, "Currency Sell History Dataset")
+            sellDataset.cubicIntensity = 0.2f
+            sellDataset.setDrawFilled(true)
+            sellDataset.lineWidth = 2.0f
+            sellDataset.color = Color.rgb(0, 200, 250)
+            sellDataset.setCircleColor(Color.rgb(0, 200, 250))
+            sellDataset.circleHoleColor = Color.rgb(0, 200, 250)
+            sellDataset.highLightColor = Color.rgb(0, 255, 0)
+            sellDataset.fillColor = Color.rgb(0, 50, 75)
+
+            val datasets = ArrayList<ILineDataSet>()
+            datasets.add(sellDataset)
+            datasets.add(buyDataset)
+
+            val data = LineData(datasets)
+            data.setValueTextSize(9.0f)
+            data.setDrawValues(false)
+
+            it.data = data
+            it.invalidate()
+
+            it.setOnChartValueSelectedListener(object : OnChartValueSelectedListener
+            {
+                override fun onNothingSelected()
+                {
+                    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                    val buyValue = currencyHistory.receiveCurrencyGraphData?.get((currencyHistory.receiveCurrencyGraphData?.size ?: 1) - 1)?.value ?: 1.0
+                    val sellValue = 1.0 / (currencyHistory.payCurrencyGraphData?.get((currencyHistory.payCurrencyGraphData?.size ?: 1) - 1)?.value ?: 1.0)
+
+                    val dateString = "${calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH)} ${calendar.get(Calendar.DAY_OF_MONTH)}"
+                    val buyValueString = "${String.format("%.1f", buyValue)} ×"
+                    val sellValueString = "${String.format("%.1f", sellValue)} ×"
+
+                    buyDateTextView?.text = dateString
+                    buyValueTextView?.text = buyValueString
+                    sellDateTextView?.text = dateString
+                    sellValueTextView?.text = sellValueString
+                }
+
+                override fun onValueSelected(e: Entry?, h: Highlight?)
+                {
+                    val datasetIdx: Int = h?.dataSetIndex ?: 0
+
+                    val daysAgo: Int = e?.x?.toInt() ?: 0
+                    val currencyValue: Float = e?.y ?: 0.0f
+
+                    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                    calendar.add(Calendar.DATE, daysAgo)
+
+                    val dateString = "${calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH)} ${calendar.get(
+                        Calendar.DAY_OF_MONTH)}"
+                    val currencyValueString = "${String.format("%.1f", currencyValue)} ×"
+
+                    if (datasetIdx == 0)
+                    {
+                        buyDateTextView?.text = dateString
+                        buyValueTextView?.text = currencyValueString
+                    }
+                    else
+                    {
+                        sellDateTextView?.text = dateString
+                        sellValueTextView?.text = currencyValueString
+                    }
+
+                }
+            })
+
+        }
     }
 }
