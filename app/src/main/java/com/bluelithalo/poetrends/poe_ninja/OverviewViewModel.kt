38c5b8af5554935a215a40452e5f58da847cfc5f
Ashwin Chakilum <azureemulation@gmail.com>
2019-09-01 15:24:14 -0500

    HOTFIX: Default league changed to Standard since Legion is ending in approx. 24 hours.

--- app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
+++ app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class OverviewViewModel : ViewModel()
 {
-    private var leagueId: String = "Legion"
+    private var leagueId: String = "Standard"
     private var overviewType: Overview.Type = Overview.Type.CURRENCY
     private var searchQuery: String = ""
 
