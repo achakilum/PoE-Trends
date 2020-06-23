86c18408df1b963731505244a0d4a3513249a28c
Ashwin Chakilum <azureemulation@gmail.com>
2020-06-23 09:07:50 -0500

    Changed default league to "Harvest".

--- app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
+++ app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class OverviewViewModel : ViewModel()
 {
-    private var leagueId: String = "Delirium"
+    private var leagueId: String = "Harvest"
     private var overviewType: Overview.Type = Overview.Type.CURRENCY
     private var searchQuery: String = ""
 
