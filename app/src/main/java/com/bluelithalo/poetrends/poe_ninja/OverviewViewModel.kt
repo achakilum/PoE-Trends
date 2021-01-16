a15922c331f46e989bbb17f139c1d6bb0f114772
Bluelit Halo <bluelithalo@gmail.com>
2021-01-16 16:03:21 -0600

    Changed default league to "Ritual".

--- app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
+++ app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class OverviewViewModel : ViewModel()
 {
-    private var leagueId: String = "Heist"
+    private var leagueId: String = "Ritual"
     private var overviewType: Overview.Type = Overview.Type.CURRENCY
     private var searchQuery: String = ""
 
