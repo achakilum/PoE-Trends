19d55328856615f5d94fcff6dbcb9dd5e085a552
Bluelit Halo <bluelithalo@gmail.com>
2021-04-22 11:52:01 -0500

    Changed default league to "Ultimatum".

--- app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
+++ app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class OverviewViewModel : ViewModel()
 {
-    private var leagueId: String = "Ritual"
+    private var leagueId: String = "Ultimatum"
     private var overviewType: Overview.Type = Overview.Type.CURRENCY
     private var searchQuery: String = ""
 
