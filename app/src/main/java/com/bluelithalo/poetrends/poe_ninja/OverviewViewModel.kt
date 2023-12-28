71243776de2546e3f35b9d44ea003375b789f458
achakilum <achakilum@outlook.com>
2023-12-28 01:24:57 -0600

    changed league to "Affliction"

--- app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
+++ app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class OverviewViewModel : ViewModel()
 {
-    private var leagueId: String = "Archnemesis"
+    private var leagueId: String = "Affliction"
     private var overviewType: Overview.Type = Overview.Type.CURRENCY
     private var searchQuery: String = ""
 
--- app/src/test/java/com/bluelithalo/poetrends/PoeNinjaUnitTest.kt
+++ app/src/test/java/com/bluelithalo/poetrends/PoeNinjaUnitTest.kt
@@ -46,12 +46,12 @@ class PoeNinjaUnitTest
     @Test
     fun currencyOverviewsAreValid()
     {
-        val currencyCall = testPoeNinjaService?.getCurrencyOverview("Archnemesis", overviewNamesByType?.get(Overview.Type.CURRENCY) ?: "")
+        val currencyCall = testPoeNinjaService?.getCurrencyOverview("Affliction", overviewNamesByType?.get(Overview.Type.CURRENCY) ?: "")
         val currencyOverview = currencyCall?.execute()?.body()
 
         assertNotNull(currencyOverview)
 
-        val fragmentCall = testPoeNinjaService?.getCurrencyOverview("Archnemesis", overviewNamesByType?.get(Overview.Type.FRAGMENT) ?: "")
+        val fragmentCall = testPoeNinjaService?.getCurrencyOverview("Affliction", overviewNamesByType?.get(Overview.Type.FRAGMENT) ?: "")
         val fragmentOverview = fragmentCall?.execute()?.body()
 
         assertNotNull(fragmentOverview)
@@ -60,7 +60,7 @@ class PoeNinjaUnitTest
     @Test
     fun currencyOverviewFilteredBySextantShowsThreeSextants()
     {
-        val currencyCall = testPoeNinjaService?.getCurrencyOverview("Archnemesis", overviewNamesByType?.get(Overview.Type.CURRENCY) ?: "")
+        val currencyCall = testPoeNinjaService?.getCurrencyOverview("Affliction", overviewNamesByType?.get(Overview.Type.CURRENCY) ?: "")
         var currencyOverview = currencyCall?.execute()?.body()?.byName("sextant")
 
         assertEquals(3, currencyOverview?.lines?.size)
